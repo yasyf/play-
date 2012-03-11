6b73130aa88850447be1ed2fed4a862a24c0c0db
Yasyf Mohamedali <yasyf@yasyf.com>
2012-03-10 22:32:18 -0800

    now updates and stores positions of BOTH players

--- gety.php
+++ gety.php
@@ -26,12 +26,12 @@ exit("game not set");
 	elseif ($player == 2) //box-dropping player
 	{ 
 	$box = $_REQUEST['box']; //box to get
-	$sql = "SELECT box".$box."y
+	$sql = "SELECT box1y,box2y,box3y
 		 FROM `yasyfcom_play`.`".$game."`
 		WHERE turn=".$turn;
 		$result = mysql_query($sql);
 		while($data = mysql_fetch_row($result)){
-		   echo("box".$box."y=$data[0]");
+		   echo("box1y=$data[0]&box2y=$data[1]&box3y=$data[2]");
 		}
 
 	}
