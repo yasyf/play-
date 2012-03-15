54213888d5707b966a13e789707eb9336e8fd250
Yasyf Mohamedali <yasyf@yasyf.com>
2012-03-15 00:04:21 -0700

    mutiplayer integration - revert here

--- setturn.php
+++ setturn.php
@@ -3,17 +3,29 @@ $game = $_REQUEST['game'];
 if (!isset($game)) {
 exit("game not set");
 }
-$host = "localhost"; //database location
-$user = "yasyfcom_play"; //database username
-$pass = "Brentwood!"; //database password
-$db_name = "yasyfcom_play"; //database name
-$link = mysql_connect($host, $user, $pass);
-mysql_select_db($db_name);
-$turn = $_REQUEST['turn']; //new (next) turn
 
-$sql = "INSERT INTO `yasyfcom_play`.`".$game."`(`box1y`, `box1x`, `box2y`, `box2x`, `box3y`, `box3x`, `player1y`, `player1x`, `turn`, `player1ready`, `player2ready`)
-(SELECT box1y, box1x, box2y, box2x, box3y, box3x, player1y, player1x, ".($turn + 1).", 0, 0
-FROM `yasyfcom_play`.`".$game."` WHERE turn=".$turn.")";
-	mysql_query($sql)or die(mysql_error());
+	$host = "localhost"; //database location
+	$user = "yasyfcom_play"; //database username
+	$pass = "Brentwood!"; //database password
+	$db_name = "yasyfcom_play"; //database name
+	$link = mysql_connect($host, $user, $pass);
+	mysql_select_db($db_name);
+	$turn = $_REQUEST['turn']; //current turn
+$sql2 =  "SELECT *
+		 FROM `yasyfcom_play`.`".$game."`
+		WHERE turn=".($turn + 1);
+		$result=@mysql_query($sql2);
+		$data = mysql_fetch_row($result);
+		
+		if (!$data)
+					{
+
+					$sql = "INSERT INTO `yasyfcom_play`.`".$game."`(`box1y`, `box1x`, `box2y`, `box2x`, `box3y`, `box3x`, `player1y`, `player1x`, `turn`, `player1ready`, `player2ready`)
+					(SELECT box1y, box1x, box2y, box2x, box3y, box3x, player1y, player1x, ".($turn + 1).", 0, 0
+					FROM `yasyfcom_play`.`".$game."` WHERE turn=".$turn.")";
+						mysql_query($sql)or die(mysql_error());
+
+					}
+
 
 ?>
