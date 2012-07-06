37599e102d025806bb9326c328f5acfd7520d9f2
Yasyf Mohamedali <yasyf@yasyf.com>
2012-07-06 01:01:16 -0700

    cloud db is blocked by firewall, revert :(

--- vars.php
+++ vars.php
@@ -1,9 +1,11 @@
 <?php
 
-$host = "instance20541.db.xeround.com:13589"; //database location
+$host = "localhost"; //database location
 $user = "yasyf_play"; //database username
-$pass = "Password!"; //database password
+$pass = "Brentwood!"; //database password
 $db_name = "yasyf_play"; //database name
 $link = mysql_connect($host, $user, $pass);
+ if (!$link)
+    die('Could not connect: ' . mysql_error());
 mysql_select_db($db_name);
 ?>
