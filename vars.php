c22be338bcc387bad37c6d4fd20d267141f8c8b1
Yasyf Mohamedali <yasyf@yasyf.com>
2012-07-06 00:22:53 -0700

    updated vars.php with new cloud db

--- vars.php
+++ vars.php
@@ -1,8 +1,8 @@
 <?php
 
-$host = "localhost"; //database location
+$host = "instance20541.db.xeround.com:13589"; //database location
 $user = "yasyf_play"; //database username
-$pass = "Brentwood!"; //database password
+$pass = "Password!"; //database password
 $db_name = "yasyf_play"; //database name
 $link = mysql_connect($host, $user, $pass);
 mysql_select_db($db_name);
